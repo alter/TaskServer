a3d5271cf59e4d69e36dcb79f07e686a7657e1be
alter <alter.pub@gmail.com>
2013-03-15 01:45:29 +0400

    added test for TQueue

--- spec/TQueue_spec.rb
+++ spec/TQueue_spec.rb
@@ -1,21 +1,37 @@
 #!/usr/bin/env ruby
-
-queue = TQueue.new
-queue.push('test1')
-queue.push('test2')
-queue.push('test3')
-queue.push('test4')
-queue.push('test5')
-
-p queue.list
-p queue.size
-queue.remove(2)
-p queue.list
-p queue.size
-queue.remove(0)
-p queue.list
-p queue.size
-queue.pull
-p queue.list
-p queue.size
-
+require 'spec_helper'
+describe TQueue do
+  before :all do
+      @queue = TQueue.new    
+  end
+  
+  it "should add 5 tasks to queue" do
+    @queue.push('test1')
+    @queue.push('test2')
+    @queue.push('test3')
+    @queue.push('test4')
+    @queue.push('test5')
+    list = @queue.list
+    puts "task list: #{list}"
+  end
+  
+  it "should remove task with index 2 and return size of queue" do
+    size = @queue.remove(2)
+    puts "queue size: #{size}"
+  end
+  
+  it "should take task from queue" do
+    current_task = @queue.pull
+    puts "current task: #{current_task}"
+  end
+  
+  it "should show list of tasks in queue" do
+    list = @queue.list
+    puts "task list: #{list}"
+  end
+  
+  it "should show size of queue" do
+    size = @queue.size
+    puts "queue size: #{size}"
+  end
+end
